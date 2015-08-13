41f2b82fb04c6751b054bd289dce5d4c075ad8b2
I am Clinton <garwoodpr@users.noreply.github.com>
2015-08-13 03:57:18 -0500

    improved unit testing
    new testing methodology

--- luhnProofTests.py
+++ luhnProofTests.py
@@ -1,41 +1,35 @@
-import luhnProofFunction
+import unittest 
+from unittest import TestCase
+from doLuhn import aLuhn
 
-tooShort = '123'
-def test_proof_too_short_fails(tooShort):
-    assert doLuhn(tooShort) == False
+class TestStringMethods(unittest.TestCase):
 
-tooLong = '123456789123456789'
-def test_proof_too_long_fails(tooLong):
-    assert doLuhn(tooLong) == False
+    def test_16_digit_proof_success(sixteenDigitLuhn):
+        TestStringMethods().assertIs(aLuhn.doLuhn('2222222222222224'), True)
 
-sixteenDigitLuhn = '2222222222222224'
-def test_16_digit_proof_success(sixteenDigitLuhn):
-    assert doLuhn(sixteenDigitLuhn) == True
+    def test_15_digit_proof_success(fifteenDigitLuhn):
+        TestStringMethods().assertIs(aLuhn.doLuhn('111111111111119'), True)
 
-sixteenDigitDud = '2222222222222222'
-def test_16_digit_proof_fails(sixteenDigitDud):
-    assert doLuhn(sixteenDigitDud) == False
+    def test_14_digit_proof_success(fourteenDigitLuhn):
+        TestStringMethods().assertIs(aLuhn.doLuhn('33333333333330'), True)
 
-fifteenDigitLuhn = '111111111111119'
-def test_15_digit_proof_success(fifteenDigitLuhn):
-    assert doLuhn(fifteenDigitLuhn) == True
+    def test_proof_too_long_fails(cardNumber):
+        TestStringMethods().assertFalse(aLuhn.doLuhn('123456789123456789'))
 
-fifteenDigitDud = '111111111111111'
-def test_15_digit_proof_fails(fifteenDigitDud):
-    assert doLuhn(fifteenDigitDud) == False
+    def test_16_digit_proof_fails(sixteenDigitDud):
+        TestStringMethods().assertFalse(aLuhn.doLuhn('2222222222222222'))
 
-fourteenDigitLuhn = '33333333333333'
-def test_14_digit_proof_success(fourteenDigitLuhn):
-    assert doLuhn(fourteenDigitLuhn) == False
+    def test_15_digit_proof_fails(fifteenDigitDud):
+        TestStringMethods().assertFalse(aLuhn.doLuhn('111111111111111'))
 
-fourteenDigitDud = '33333333333330'
-def test_14_digit_proof_fails(fourteenDigitDud):
-    assert doLuhn(fourteenDigitDud) == True
+    def test_14_digit_proof_fails(fourteenDigitDud):
+        TestStringMethods().assertFalse(aLuhn.doLuhn('33333333333333'))
 
-anyAlphaFails = '3a333333333330'
-def test_14_digit_proof_fails(anyAlphaFails):
-    assert doLuhn(anyAlphaFails) == False
+    def test_14_digit_proof_fails(anyAlphaFails):
+        TestStringMethods().assertFalse(aLuhn.doLuhn('3a333333333330'))
 
-anythinButDigitsFails = '33;3333333330'
-def test_14_digit_proof_fails(anythinButDigitsFails):
-    assert doLuhn(anythinButDigitsFails) == False
+    def test_14_digit_proof_fails(anythinButDigitsFails):
+        TestStringMethods().assertFalse(aLuhn.doLuhn('33.3333333330'))
+
+    def test_proof_too_short_fails(cardNumber):
+        TestStringMethods().assertFalse(aLuhn.doLuhn('123'))
