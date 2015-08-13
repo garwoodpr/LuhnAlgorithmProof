bd6fc6e2c4c58ac9894a035dc0cd7314aaeff890
I am Clinton <garwoodpr@users.noreply.github.com>
2015-08-13 04:21:03 -0500

    update with comments
     and removed simple print statements

--- luhnProofFunction.py
+++ luhnProofFunction.py
@@ -8,11 +8,11 @@
 
 def doLuhn(cardNumber):
     cardLength = str(cardNumber)
-    cardLength = len(cardLength)
     try: 
         cardNumbers = int(cardNumber)
     except ValueError:
         return(False)
+    cardLength = len(cardLength)
     everyOtherFromFarRightFor16 = [-2,-4,-6,-8,-10,-12,-14,-16]
     everyOtherFromFarRightFor15 = [-2,-4,-6,-8,-10,-12,-14]
     everyOddFromFarRightButOneFor15 = [-3,-5,-7,-9,-11,-13,-15]
@@ -21,6 +21,7 @@ def doLuhn(cardNumber):
     doubleSet = []
     addUpDoubles = 0
     addUpOthers = 0
+    # setup the counting variables for appropriate card lengths
     if (cardLength == 16):
         doubleList = everyOtherFromFarRightFor16
         addUpTheOddDigits = everyOddFromFarRightButOneFor15
@@ -32,30 +33,35 @@ def doLuhn(cardNumber):
         addUpTheOddDigits = everyOddFromFarRightButOneFor14
     else:
         return(False)
+    # select the items for doubling
     for each in doubleList:
         doubleThis = cardNumber[each]
         doubleThis = int(doubleThis) * 2
         nowDoubled = str(doubleThis)
+        # add single digit items to the doubleSet
         if (len(nowDoubled) == 1):
             nowDoubled = nowDoubled
             doubleSet.append(nowDoubled)
         else:
+            # add each digit of 2-digit items to each other 
+            # and then add each item to the the doubleSet
             db1, db2 = nowDoubled[0], nowDoubled[1]
             db1, db2 = int(db1), int(db2)
             dbladd = db1 + db2
             doubleSet.append(dbladd)
     for each in doubleSet:
         addUpDoubles += int(each)
-    print('addUpDoubles:', addUpDoubles)
+    # add all items in together in the doubleSet
     for each in addUpTheOddDigits:
         addOther = cardNumber[each]
         otherToAdd = int(addOther)
         addUpOthers += otherToAdd
-    print('addUpOthers:', addUpOthers)
+    # add all items left over from the original cardNumber
     totalSum = int(addUpDoubles) + int(addUpOthers)
-    print('totalSum:', totalSum)
+    # take totalSum * 9, then take Modulus of that number by 10
     totalSumTimesNine = (totalSum * 9)
     modTheTotalSum = (totalSumTimesNine % 10)
+    # compare modTheTotalSum to the right-most digit in cardNumber 
     if (str(modTheTotalSum) == cardNumber[-1]):
         return(True)
     else:
